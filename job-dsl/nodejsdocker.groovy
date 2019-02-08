640a5adc9335dbf902a01a701ac32edf9e227e60
Juan Pablo Perez <juanp_perez@loyaltycrm.com>
2019-02-08 19:26:08 +0000

    Put the proper GitHub repository

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/peelmicro/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('peelmicro')
             node / gitConfigEmail('juanp_perez@loyaltycrm.com')
         }
