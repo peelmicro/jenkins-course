2750d24f8b29e8b276f70eb76280e33ffc2cf8e0
Juan Pablo Perez <juanp_perez@loyaltycrm.com>
2019-02-08 19:11:17 +0000

    Updated nodejs.groovy to put my own credentials

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/peelmicro/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('peelmicro')
+            node / gitConfigEmail('juanp_perez@loyaltycrm.com')
         }
     }
     triggers {
